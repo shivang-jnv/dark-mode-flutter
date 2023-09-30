8f1cc6fbc114d7d16d815c2b3cf4db16e846a9f1
shivang-jnv <skjnvspn@gmail.com>
2023-09-30 12:24:20 +0530

    system and manual dark-light  mode switching app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:dark_mode/pages/homepage.dart';
+import 'package:dark_mode/theme/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:dark_mode/theme/theme.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
